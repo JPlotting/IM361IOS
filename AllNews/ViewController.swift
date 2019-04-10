5707850a1a6e76318d1c4d8746478e7d9a8c0df8
Jakob Plotts <jakobplotts@yahoo.com>
2019-04-10 00:53:27 -0500

    Initial Commit

---
+++ AllNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllNews
+//
+//  Created by Jakob Plotts on 4/10/19.
+//  Copyright © 2019 Jakob Plotts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
